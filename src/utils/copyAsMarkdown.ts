e84ea32904f1e083e770883c5c05797f02e91c1b
Brian <to.hoang.long@moneyforward.co.jp>
2025-08-13 22:18:02 +0700

    fix header

--- src/utils/copyAsMarkdown.ts
+++ src/utils/copyAsMarkdown.ts
@@ -1,13 +1,23 @@
 import TurndownService from "turndown";
 
-const turndownService = new TurndownService();
+const turndownService = new TurndownService({
+  headingStyle: "atx",
+});
+
+turndownService.addRule("image", {
+  filter: ["img", "picture", "figure"],
+  replacement: function (content, node) {
+    console.log("node", content);
+    const div = document.createElement("div");
+    div.appendChild(node);
+    const img = div.querySelector("img");
+    const src = img?.getAttribute("src") || "";
+    return src ? `![](${src})` : content;
+  },
+});
 
 export const copyHtmlToMarkdown = async (htmlContent: string) => {
-  let markdownContent = turndownService.turndown(htmlContent); // Convert to Markdown
-  markdownContent = markdownContent.replace(
-    /!\[[^\]]*\]\(([^)]+)\)/g,
-    "![]($1)"
-  ); // Replace all ![alt](image) with ![](image)
+  const markdownContent = turndownService.turndown(htmlContent); // Convert to Markdown
 
   try {
     await navigator.clipboard.writeText(markdownContent);
